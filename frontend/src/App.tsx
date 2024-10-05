cc901663167a83fe46a1877b6a0db7363dc03577
Rayyan Shaik <rayyanshaik2022@gmail.com>
2024-10-05 14:15:45 -0400

    Added Mantine

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+import Signup from "./pages/Signup";
+
 function App() {
   return (
     <>
-      <h1>test</h1>
+      <MantineProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path="/" element={<Login />} /> */}
+            <Route path="/signup" element={<Signup />} />
+            {/* Example of dynamic route */}
+            {/* <Route path="/profile/:username/" element={<Profile />} /> */}
+            {/* Example of default route */}
+            {/* <Route path="*" element={<Login />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </MantineProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
