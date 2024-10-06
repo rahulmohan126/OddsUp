4e54170f2dbb356646bb15071f02ec45fe9a04ec
Rahul Mohan <45293734+rahulmohan126@users.noreply.github.com>
2024-10-06 07:55:52 -0400

    Added middleware (optional using EXPRESS_MIDDLEWARE env variable)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import authenticateJWT from "./middleware/auth";
+import errorMiddleware from "./middleware/error";
 import dotenv from "dotenv";
 dotenv.config()
 
@@ -21,10 +23,17 @@ app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.EXPRESS_MIDDLEWARE) {
+  app.use(<express.Handler>authenticateJWT);
+}
 
 app.use('/api/user', require('./routes/user'));
 app.use('/api/group', require('./routes/group'));
 app.use('/api/challenge', require('./routes/challenge'));
+
+if (process.env.EXPRESS_MIDDLEWARE) {
+  app.use(errorMiddleware);
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
